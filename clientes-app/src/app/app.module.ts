1b4c2e9a4002024ed95b814c1ea65242f25c563f
gersondlarosa <gerson.dlarosa@gmail.com>
2022-05-25 20:00:05 -0500

    add clientes.component
    Se agrega clientes componet en front para generar los json correspondientes

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
---
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {Cliente} from './cliente';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html'
+})
+export class ClientesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
