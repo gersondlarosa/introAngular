328936ecfb446cb0a5a1a3897fffc201e6ebf55e
gersondlarosa <gerson.dlarosa@gmail.com>
2022-05-27 11:53:45 -0500

    add
    add

--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {Cliente} from './cliente';
 
 @Component({
   selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
+
+cliente: Cliente[] = [
+  {id: 1,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"},
+  {id: 2,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"},
+  {id: 3,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"},
+  {id: 4,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"},
+  {id: 5,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"},
+  {id: 6,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"},
+  {id: 7,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"},
+  {id: 8,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"},
+  {id: 9,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"},
+  {id: 10,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"},
+  {id: 11,nombre:"Joel Gerson", apellido: "Flores de la Rosa ", email:'gerson.dlarosa@gmail.com', createAt:"27/05/200"}
+
+];
 
   constructor() { }
 
